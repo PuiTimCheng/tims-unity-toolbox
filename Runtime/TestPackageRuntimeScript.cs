a5b847765fa9769b69ff84539a2d83be1015c3b2
Tim <chengpuitim@163.com>
2024-03-08 00:09:29 +0800

    fix: test semantic versioning

--- Runtime/TestPackageRuntimeScript.cs
+++ Runtime/TestPackageRuntimeScript.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class TestPackageRuntimeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //test semantic versioning
 }
