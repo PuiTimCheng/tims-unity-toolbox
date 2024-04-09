4625c9350aa3fbf6e10da00e1ef42c3031a3fa6a
Tim <chengpuitim@163.com>
2024-04-09 23:43:19 +0800

    add event bus scripts

---
+++ Runtime/TimToolBox/DesignPattern/EventBus/Events.cs
@@ -0,0 +1,9 @@
+﻿namespace TimToolBox.DesignPattern.EventBus {
+    public interface IEvent { }
+    
+    public struct TestEvent : IEvent {
+    }
+    public struct TestIntEvent : IEvent {
+        public int Val;
+    }
+}
