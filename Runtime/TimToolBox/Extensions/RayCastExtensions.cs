fd3176541a6e5978e1904cdd3ab2afa6c1eee580
郑倍添Tim <chengpuitim@163.com>
2024-04-30 18:33:48 +0800

    added move along path test script

--- Runtime/TimToolBox/Extensions/RayCastExtensions.cs
+++ Runtime/TimToolBox/Extensions/RayCastExtensions.cs
@@ -6,11 +6,11 @@ namespace TimToolBox.Extensions
 {
     public static class RayCastExtensions
     {
-        public static bool CameraMainRaycastFirstHitPos(Vector2 screenPos, out Vector3 worldPos, int layerMask = -1)
+        public static bool CameraMainRaycastFirstHitPos(Vector2 screenPos, out Vector3 worldPos, int ignoreLayers = -1)
         {
             Ray ray = Camera.main.ScreenPointToRay(screenPos);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ignoreLayers))
             {
                 worldPos = hit.point;
                 return true;
