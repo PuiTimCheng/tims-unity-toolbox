9dac1a7182d748f4801d8110b2dcd178c4815c76
Tim <chengpuitim@163.com>
2024-08-03 00:43:51 +0800

    Changed SetUp code

--- Editor/ProjectSetUp.cs
+++ Editor/ProjectSetUp.cs
@@ -1,189 +1,146 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 using UnityEditor;
-using UnityEngine;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+using static System.Environment;
 using static System.IO.Path;
 using static UnityEditor.AssetDatabase;
 
-namespace TimToolBox
-{
-    public static class ProjectSetUp
-    {
-        [MenuItem("Tim'sToolBox/Setup/Create Default Folders")]
-        public static void CreateDefaultFolders()
+public static class ProjectSetup {
+    [MenuItem("Tim'sToolBox/Setup/Import Essential Assets")]
+    public static void ImportEssentials() {
+        //Assets.ImportAsset("Odin Inspector and Serializer.unitypackage", "Sirenix/Editor ExtensionsSystem");
+        //Assets.ImportAsset("Odin Validator.unitypackage", "Sirenix/Editor ExtensionsUtilities");
+        //Assets.ImportAsset("Editor Console Pro.unitypackage", "FlyingWorm/Editor ExtensionsSystem");
+        // and so on...
+        var folderPath = "Packages/com.ptcheng.toolbox/DownloadedPackage/EssentialPackages";
+        // Get all files with a .unitypackage extension in the specified folder
+        string[] packageFiles = Directory.GetFiles(folderPath, "*.unitypackage");
+        // Display the package names
+        foreach (var packageFileName in packageFiles)
         {
-            Folders.CreateDefault("_Project", "Animation", "Art", "Materials", "Prefabs", "Scripts/Core","Scripts/ScriptableObjects",
-                "Scripts/UI");
-            Refresh();
+            Debug.Log("Importing Unity Package Name: " + GetFileNameWithoutExtension(packageFileName));
+            Assets.ImportAssetFromPath(packageFileName);
         }
+    }
 
-        [MenuItem("Tim'sToolBox/Setup/Import Must Have Folders")]
-        public static void ImportMustHavePackages()
-        {
-            try
-            {
-                var folderPath = "Packages/com.ptcheng.toolbox/unityPackage/MustHavePackages";
-                // Get all files with a .unitypackage extension in the specified folder
-                string[] packageFiles = Directory.GetFiles(folderPath, "*.unitypackage");
-                // Display the package names
-                foreach (var packageFileName in packageFiles)
-                {
-                    Debug.Log("Unity Package Name: " + GetFileNameWithoutExtension(packageFileName));
-                    Assets.ImportAsset(packageFileName, "", "",false);
-                }
+    [MenuItem("Tim'sToolBox/Setup/Install Essential Packages")]
+    public static void InstallPackages() {
+        Packages.InstallPackages(new[] {
+            //"com.unity.2d.animation",
+            //"git+https://github.com/adammyhre/Unity-Utils.git",
+            //"git+https://github.com/adammyhre/Unity-Improved-Timers.git",
+            "com.unity.addressables",
+            "com.unity.cinemachine",
+            "com.unity.ai.navigation",
+            // If necessary, import new Input System last as it requires a Unity Editor restart
+            "com.unity.inputsystem",
+        });
+    }
+
+    [MenuItem("Tim'sToolBox/Setup/Create Folders")]
+    public static void CreateFolders() {
+        Folders.Create("_Project", "Animation", "Art", "Materials", "Prefabs", "Scripts/Tests", "Scripts/Tests/Editor", "Scripts/Tests/Runtime");
+        Refresh();
+        Folders.Move("_Project", "Scenes");
+        Folders.Move("_Project", "Settings");
+        Folders.Delete("TutorialInfo");
+        Refresh();
+
+        MoveAsset("Assets/InputSystem_Actions.inputactions", "Assets/_Project/Settings/InputSystem_Actions.inputactions");
+        DeleteAsset("Assets/Readme.asset");
+        Refresh();
+    }
+
+    static class Assets {
+        public static void ImportAsset(string asset, string folder) {
+            var basePath = GetFolderPath(SpecialFolder.ApplicationData);
+            var assetsFolder = Combine(basePath, "Unity/Asset Store-5.x");
+            asset = asset.EndsWith(".unitypackage") ? asset : asset + ".unitypackage";
+            ImportPackage(Combine(assetsFolder, folder, asset), false);
+        }
+        public static void ImportAssetFromPath(string path) {
+            var rootFolder = GetDirectoryName(Application.dataPath);
+            string absolute =   Path.GetFullPath(path);
+            var combine = Combine(rootFolder, "", path);
+            ImportPackage(combine, false);
+        }
+    }
+
+    static class Packages {
+        static AddRequest request;
+        static Queue<string> packagesToInstall = new Queue<string>();
+
+        public static void InstallPackages(string[] packages) {
+            foreach (var package in packages) {
+                packagesToInstall.Enqueue(package);
             }
-            catch (Exception e)
-            {
-                Debug.LogError("Error retrieving package names: " + e.Message);
+
+            if (packagesToInstall.Count > 0) {
+                StartNextPackageInstallation();
             }
         }
 
+        static async void StartNextPackageInstallation() {
+            request = Client.Add(packagesToInstall.Dequeue());
+            
+            while (!request.IsCompleted) await Task.Delay(10);
+            
+            if (request.Status == StatusCode.Success) Debug.Log("Installed: " + request.Result.packageId);
+            else if (request.Status >= StatusCode.Failure) Debug.LogError(request.Error.message);
 
-        [MenuItem("Tim'sToolBox/Setup/Install Unity Addressables")]
-        public static void InstallAddressables() {
-            Packages.InstallPackages(new[]
-            {
-                "com.unity.addressables",
-            });
+            if (packagesToInstall.Count > 0) {
+                await Task.Delay(1000);
+                StartNextPackageInstallation();
+            }
         }
-        [MenuItem("Tim'sToolBox/Setup/Install Unity AINavigation")]
-        public static void InstallUnityAINavigation()
-        {
-            Packages.InstallPackages(new[]
-            {
-                "com.unity.ai.navigation"
-            });
-        }
-        [MenuItem("Tim'sToolBox/Setup/Install Unity InputSystem")]
-        public static void InstallInputSystem() {
-            Packages.InstallPackages(new[]
-            {
-                "com.unity.inputsystem",
-            });
-        }
-        [MenuItem("Tim'sToolBox/Setup/Import Unity Cinemachine")]
-        public static void ImportCinemachine()
-        {
-            Packages.InstallPackages(new[]
-            {
-                "com.unity.cinemachine",
-            });
+    }
+
+    static class Folders {
+        public static void Create(string root, params string[] folders) {
+            var fullpath = Combine(Application.dataPath, root);
+            if (!Directory.Exists(fullpath)) {
+                Directory.CreateDirectory(fullpath);
+            }
+
+            foreach (var folder in folders) {
+                CreateSubFolders(fullpath, folder);
+            }
         }
         
+        static void CreateSubFolders(string rootPath, string folderHierarchy) {
+            var folders = folderHierarchy.Split('/');
+            var currentPath = rootPath;
+
+            foreach (var folder in folders) {
+                currentPath = Combine(currentPath, folder);
+                if (!Directory.Exists(currentPath)) {
+                    Directory.CreateDirectory(currentPath);
+                }
+            }
+        }
         
-        [MenuItem("Tim'sToolBox/Setup/Install ParrelSync")]
-        public static void InstallParrelSync() {
-            Packages.InstallPackages(new[]
-            {
-                "git+https://github.com/VeriorPies/ParrelSync.git?path=/ParrelSync"
-            });
-        }
-        //[MenuItem("Tools/Setup/Install Netcode for GameObjects")]
-        public static void InstallNetcodeForGameObjects()
-        {
-            Packages.InstallPackages(new[]
-            {
-                "com.unity.multiplayer.tools",
-                "com.unity.netcode.gameobjects"
-            });
-        }
+        public static void Move(string newParent, string folderName) {
+            var sourcePath = $"Assets/{folderName}";
+            if (IsValidFolder(sourcePath)) {
+                var destinationPath = $"Assets/{newParent}/{folderName}";
+                var error = MoveAsset(sourcePath, destinationPath);
 
-        //[MenuItem("Tools/Setup/Install My Favorite Open Source")]
-        public static void InstallOpenSource()
-        {
-            Packages.InstallPackages(new[]
-            {
-                "git+https://github.com/KyleBanks/scene-ref-attribute",
-                "git+https://github.com/starikcetin/Eflatun.SceneReference.git#3.1.1"
-            });
-        }
-
-        static class Folders
-        {
-            public static void CreateDefault(string root, params string[] folders)
-            {
-                var fullpath = Path.Combine(Application.dataPath, root);
-                if (!Directory.Exists(fullpath))
-                {
-                    Directory.CreateDirectory(fullpath);
-                }
-
-                foreach (var folder in folders)
-                {
-                    CreateSubFolders(fullpath, folder);
-                }
-            }
-
-            private static void CreateSubFolders(string rootPath, string folderHierarchy)
-            {
-                var folders = folderHierarchy.Split('/');
-                var currentPath = rootPath;
-                foreach (var folder in folders)
-                {
-                    currentPath = Path.Combine(currentPath, folder);
-                    if (!Directory.Exists(currentPath))
-                    {
-                        Directory.CreateDirectory(currentPath);
-                    }
+                if (!string.IsNullOrEmpty(error)) {
+                    Debug.LogError($"Failed to move {folderName}: {error}");
                 }
             }
         }
+        
+        public static void Delete(string folderName) {
+            var pathToDelete = $"Assets/{folderName}";
 
-        static class Packages
-        {
-            static AddRequest Request;
-            static Queue<string> PackagesToInstall = new();
-
-            public static void InstallPackages(string[] packages)
-            {
-                foreach (var package in packages)
-                {
-                    PackagesToInstall.Enqueue(package);
-                }
-
-                // Start the installation of the first package
-                if (PackagesToInstall.Count > 0)
-                {
-                    Request = Client.Add(PackagesToInstall.Dequeue());
-                    EditorApplication.update += Progress;
-                }
-            }
-
-            static async void Progress()
-            {
-                if (Request.IsCompleted)
-                {
-                    if (Request.Status == StatusCode.Success)
-                        Debug.Log("Installed: " + Request.Result.packageId);
-                    else if (Request.Status >= StatusCode.Failure)
-                        Debug.Log(Request.Error.message);
-
-                    EditorApplication.update -= Progress;
-
-                    // If there are more packages to install, start the next one
-                    if (PackagesToInstall.Count > 0)
-                    {
-                        // Add delay before next package install
-                        await Task.Delay(1000);
-                        Request = Client.Add(PackagesToInstall.Dequeue());
-                        EditorApplication.update += Progress;
-                    }
-                }
-            }
-        }
-
-        static class Assets
-        {
-            public static void ImportAsset(string asset, string subfolder, string rootFolder = null,bool interactive = false)
-            {
-                if(rootFolder == null) rootFolder = GetDirectoryName(Application.dataPath);
-                ImportPackage(Combine(rootFolder, subfolder, asset), interactive);
+            if (IsValidFolder(pathToDelete)) {
+                DeleteAsset(pathToDelete);
             }
         }
     }
---
+++ Editor/ProjectSetUpOld.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using static System.IO.Path;
+using static UnityEditor.AssetDatabase;
+
+namespace TimToolBox
+{
+    public static class ProjectSetUpOld
+    {
+        //[MenuItem("Tim'sToolBox/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+            Folders.CreateDefault("_Project", "Animation", "Art", "Materials", "Prefabs", "Scripts/Core","Scripts/ScriptableObjects",
+                "Scripts/UI");
+            Refresh();
+        }
+
+        //[MenuItem("Tim'sToolBox/Setup/Import Must Have Folders")]
+        public static void ImportMustHavePackages()
+        {
+            try
+            {
+                var folderPath = "Packages/com.ptcheng.toolbox/unityPackage/MustHavePackages";
+                // Get all files with a .unitypackage extension in the specified folder
+                string[] packageFiles = Directory.GetFiles(folderPath, "*.unitypackage");
+                // Display the package names
+                foreach (var packageFileName in packageFiles)
+                {
+                    Debug.Log("Unity Package Name: " + GetFileNameWithoutExtension(packageFileName));
+                    Assets.ImportAsset(packageFileName, "", "",false);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error retrieving package names: " + e.Message);
+            }
+        }
+
+
+        //[MenuItem("Tim'sToolBox/Setup/Install Unity Addressables")]
+        public static void InstallAddressables() {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.addressables",
+            });
+        }
+        //[MenuItem("Tim'sToolBox/Setup/Install Unity AINavigation")]
+        public static void InstallUnityAINavigation()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.ai.navigation"
+            });
+        }
+        //[MenuItem("Tim'sToolBox/Setup/Install Unity InputSystem")]
+        public static void InstallInputSystem() {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.inputsystem",
+            });
+        }
+        //[MenuItem("Tim'sToolBox/Setup/Import Unity Cinemachine")]
+        public static void ImportCinemachine()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.cinemachine",
+            });
+        }
+        
+        
+        //[MenuItem("Tim'sToolBox/Setup/Install ParrelSync")]
+        public static void InstallParrelSync() {
+            Packages.InstallPackages(new[]
+            {
+                "git+https://github.com/VeriorPies/ParrelSync.git?path=/ParrelSync"
+            });
+        }
+        //[MenuItem("Tools/Setup/Install Netcode for GameObjects")]
+        public static void InstallNetcodeForGameObjects()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "com.unity.multiplayer.tools",
+                "com.unity.netcode.gameobjects"
+            });
+        }
+
+        //[MenuItem("Tools/Setup/Install My Favorite Open Source")]
+        public static void InstallOpenSource()
+        {
+            Packages.InstallPackages(new[]
+            {
+                "git+https://github.com/KyleBanks/scene-ref-attribute",
+                "git+https://github.com/starikcetin/Eflatun.SceneReference.git#3.1.1"
+            });
+        }
+
+        static class Folders
+        {
+            public static void CreateDefault(string root, params string[] folders)
+            {
+                var fullpath = Path.Combine(Application.dataPath, root);
+                if (!Directory.Exists(fullpath))
+                {
+                    Directory.CreateDirectory(fullpath);
+                }
+
+                foreach (var folder in folders)
+                {
+                    CreateSubFolders(fullpath, folder);
+                }
+            }
+
+            private static void CreateSubFolders(string rootPath, string folderHierarchy)
+            {
+                var folders = folderHierarchy.Split('/');
+                var currentPath = rootPath;
+                foreach (var folder in folders)
+                {
+                    currentPath = Path.Combine(currentPath, folder);
+                    if (!Directory.Exists(currentPath))
+                    {
+                        Directory.CreateDirectory(currentPath);
+                    }
+                }
+            }
+        }
+
+        static class Packages
+        {
+            static AddRequest Request;
+            static Queue<string> PackagesToInstall = new();
+
+            public static void InstallPackages(string[] packages)
+            {
+                foreach (var package in packages)
+                {
+                    PackagesToInstall.Enqueue(package);
+                }
+
+                // Start the installation of the first package
+                if (PackagesToInstall.Count > 0)
+                {
+                    Request = Client.Add(PackagesToInstall.Dequeue());
+                    EditorApplication.update += Progress;
+                }
+            }
+
+            static async void Progress()
+            {
+                if (Request.IsCompleted)
+                {
+                    if (Request.Status == StatusCode.Success)
+                        Debug.Log("Installed: " + Request.Result.packageId);
+                    else if (Request.Status >= StatusCode.Failure)
+                        Debug.Log(Request.Error.message);
+
+                    EditorApplication.update -= Progress;
+
+                    // If there are more packages to install, start the next one
+                    if (PackagesToInstall.Count > 0)
+                    {
+                        // Add delay before next package install
+                        await Task.Delay(1000);
+                        Request = Client.Add(PackagesToInstall.Dequeue());
+                        EditorApplication.update += Progress;
+                    }
+                }
+            }
+        }
+
+        static class Assets
+        {
+            public static void ImportAsset(string asset, string subfolder, string rootFolder = null,bool interactive = false)
+            {
+                if(rootFolder == null) rootFolder = GetDirectoryName(Application.dataPath);
+                ImportPackage(Combine(rootFolder, subfolder, asset), interactive);
+            }
+        }
+    }
+}
